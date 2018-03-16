781986df04f6190d7c31716e152bc567ca988aac
Henriette <Hlaursen86@gmail.com>
2018-03-16 09:09:37 +0100

    Working Runsort and FancyRunsort

---
+++ FancyRunsort.java
@@ -0,0 +1,106 @@
+import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
+
+
+public class FancyRunsort {
+
+  public FancyRunsort() {
+	}
+
+	public static void main(String[] args){
+		String[] myArray = StdIn.readAllStrings();
+    // Stopwatch timer = new Stopwatch();
+		sort(myArray);
+		// double elapsedTime = timer.elapsedTime();
+		show(myArray);
+		// StdOut.printf("Elapsed time, sorting: %f\n", elapsedTime);
+		// elapsedTime = timer.elapsedTime();
+		// StdOut.printf("Elapsed time, printing: %f\n", elapsedTime);
+	}
+
+	//Inspired by http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/7-b147/java/util/TimSort.java#TimSort.minRunLength%28int%29
+	private static int findRunLength(Comparable[] a, int lo, int hi) {
+		//Find the length of the run starting at position lo and running no further than position hi in an array of comparables
+		int runHi = lo+1;
+		if (runHi==hi) return 1;
+
+		//Find end of run
+		while (runHi < hi && less(a[runHi-1], a[runHi])) {
+			runHi++;
+		}
+
+		//Return length of run
+		return runHi - lo;
+	}
+
+	//Inspired by http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/7-b147/java/util/TimSort.java#TimSort.minRunLength%28int%29
+	public static void sort(Comparable[] a){
+	int N = a.length;
+
+	//Create a queue to keep track of the runs
+	Queue<int[]> q = new Queue<int[]>();
+
+	//First off, find all the runs in the unsorted array
+	int runCount = 0;
+	int j = 0;
+	while (j<N) {
+			int[] tempArray = new int[2];
+			tempArray[0] = j;												//Start position
+			tempArray[1] = findRunLength(a, j, N);	//Length of run
+      q.enqueue(tempArray);
+			j=j+tempArray[1];												//Start next round from startPosition + runLength
+			runCount++;
+		}
+
+  //Sort
+	Comparable[] aux = new Comparable[N];
+	while (runCount>1) {
+		int k = 0;
+		while (k<runCount/2) {
+			int[] runOne = q.dequeue();
+			int[] runTwo = q.dequeue();
+			int lo = runOne[0];
+			int mid = runTwo[0] - 1;
+			int hi = mid + runTwo[1];
+      //Insertion sort implementation for small runs
+      if (hi-lo <= 8) Insertion.sort(a, lo, hi+1);
+      else merge(a,aux, lo, mid, hi);
+
+      runOne[1] = runOne[1] + runTwo[1];			//Update runLength
+			q.enqueue(runOne);
+			k++;
+		}
+		if (runCount%2 == 1) {										//Handles uneven number of runs
+			q.enqueue(q.dequeue());
+		}
+		runCount-= k;
+	}
+}
+
+	//merge() from algs4 MergeBU
+  private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+			for (int k = lo; k <= hi; k++) {
+          aux[k] = a[k];
+      }
+      int i = lo, j = mid+1;
+      for (int k = lo; k <= hi; k++) {
+          if      (i > mid)              a[k] = aux[j++];
+          else if (j > hi)               a[k] = aux[i++];
+          else if (less(aux[j], aux[i])) a[k] = aux[j++];
+          else                           a[k] = aux[i++];
+      }
+  }
+
+  //less() from algs4 MergeBU
+	private static boolean less(Comparable v, Comparable w) {
+      return v.compareTo(w) < 0;
+  }
+
+  //show() from algs4 MergeBU
+  public static void show(Comparable[] sortedArray){
+  	for (int i = 0; i < sortedArray.length; i++){
+		System.out.println(sortedArray[i]);
+		}
+	}
+
+}
--- Runsort.java
+++ Runsort.java
@@ -3,97 +3,103 @@ import java.util.Arrays;
 
 public class Runsort {
 
-private static 	Comparable[] aux;
-private static String[] myArray;
-
 	public Runsort() {
-
 	}
 
 	public static void main(String[] args){
-		String[] tempArray = StdIn.readAllLines();
-		// int l = tempArray.length;
-		// for (int i=0; i<l; i++) {
-		// 	StdOut.println(tempArray[i]);
-		// }
-		// StdOut.println("------");
-		myArray = tempArray; //Why not just use myArray?
+		String[] myArray = StdIn.readAllStrings();
+		// Stopwatch timer = new Stopwatch();
 		sort(myArray);
-		showSortedArray(myArray);
+		// double elapsedTime = timer.elapsedTime();
+		show(myArray);
+		// StdOut.printf("Elapsed time, sorting: %f\n", elapsedTime);
+		// elapsedTime = timer.elapsedTime();
+		// StdOut.printf("Elapsed time, printing: %f\n", elapsedTime);
 	}
 
 	//Inspired by http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/7-b147/java/util/TimSort.java#TimSort.minRunLength%28int%29
-
-	private static int findRunLength(comparable[] a, int lo, int hi) {
+	private static int findRunLength(Comparable[] a, int lo, int hi) {
 		//Find the length of the run starting at position lo and running no further than position hi in an array of comparables
-		int runHi = lo+1;
-		if (runHi==hi) return 1;
+			int runHi = lo+1;
+			if (runHi==hi) return 1;
 
 		//Find end of run
-		while (runHi < hi && less(a[runHi], a[runHi-1])) {
-			runHi++;
+			while (runHi < hi && less(a[runHi-1], a[runHi])) {
+				runHi++;
+			}
 
 		//Return length of run
-		return runHi - lo;
-		}
+			return runHi - lo;
 	}
 
 	//Inspired by http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/7-b147/java/util/TimSort.java#TimSort.minRunLength%28int%29
-	private static void pushRun(Stack stack, int runBase, int runLen) {
-		int[] a = {runBase, runLen};
-		stack.push()
-	}
-
-	private static boolean isSorted(comparable[] a) {
-		int N = a.length;
-		int runLength = findRunLength(a, 0, N-1);
-		return (runlength == 1);
-	}
-
 	public static void sort(Comparable[] a){
 	int N = a.length;
+
 	//Create a queue to keep track of the runs
-	Stack runStack = new Stack();
-	//Find the ascending runs and push to stack
+	Queue<int[]> q = new Queue<int[]>();
 
-
-	//While array is not sorted, find and merge runs
-	while (!isSorted()) {
-
-	}
-
-
-	aux = new Comparable[N];
-	for (int sz = 1; sz < N; sz = sz+sz)
-    	for (int lo = 0; lo < N-sz; lo += sz+sz)
-    		merge(a, aux, lo, lo+sz-1, Math.min(lo+sz+sz-1, N-1));
-	}
-
-	// Merges the array - MergeBU from section 2.2
-    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
-        }
-
-        int i = lo, j = mid+1;
-        for (int k = lo; k <= hi; k++) {
-            if      (i > mid)              a[k] = aux[j++];
-            else if (j > hi)               a[k] = aux[i++];
-            else if (less(aux[j], aux[i])) a[k] = aux[j++];
-            else                           a[k] = aux[i++];
-        }
-
-    }
-    // Comparing elements - MergeBU from section 2.2
-    private static boolean less(Comparable a, Comparable b) {
-        String h = a.toString();
-        String i = b.toString();
-        return h.compareTo(i) < 0;
-    }
-
-    public static void showSortedArray(Comparable[] sorted){
-    	for (int i = 0; i < myArray.length; i++){
-			System.out.println(myArray[i]);
+	//First off, find all the runs in the unsorted array
+	int runCount = 0;
+	int j = 0;
+	while (j<N) {
+			int[] tempArray = new int[2];
+			tempArray[0] = j;												//Start position
+			tempArray[1] = findRunLength(a, j, N);	//Length of run
+			q.enqueue(tempArray);
+			j=j+tempArray[1];												//Start next round from startPosition + runLength
+			runCount++;
 		}
-    }
+
+	//Merge neighbouring runs for each pass through
+	Comparable[] aux = new Comparable[N];
+	while (runCount>1) {
+		int k = 0;
+		while (k<runCount/2) {
+			int[] runOne = q.dequeue();
+			int[] runTwo = q.dequeue();
+			int lo = runOne[0];
+			int mid = runTwo[0] - 1;
+			int hi = mid + runTwo[1];
+			merge(a,aux, lo, mid, hi);
+			runOne[1] = runOne[1] + runTwo[1];			//Update runLength
+			q.enqueue(runOne);
+			k++;
+		}
+		if (runCount%2 == 1) {										//Handles uneven number of runs
+			q.enqueue(q.dequeue());
+		}
+		runCount-= k;
+	}
+}
+
+	//merge() from algs4 MergeBU
+  private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+			// copy to aux[]
+			for (int k = lo; k <= hi; k++) {
+          aux[k] = a[k];
+      }
+
+			// merge back to a[]
+      int i = lo, j = mid+1;
+      for (int k = lo; k <= hi; k++) {
+          if      (i > mid)              a[k] = aux[j++];
+          else if (j > hi)               a[k] = aux[i++];
+          else if (less(aux[j], aux[i])) a[k] = aux[j++];
+          else                           a[k] = aux[i++];
+      }
+  }
+
+	//less() from algs4 MergeBU
+	private static boolean less(Comparable v, Comparable w) {
+      return v.compareTo(w) < 0;
+  }
+
+	//show() from algs4 MergeBU
+  public static void show(Comparable[] sortedArray){
+  	for (int i = 0; i < sortedArray.length; i++){
+		System.out.println(sortedArray[i]);
+		}
+	}
+
 }
